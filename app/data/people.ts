a6e8803415f771b017c8ebf11083790bbfa9ffcc
Pierfrancesco Beneventano <pierbene96@gmail.com>
2026-03-23 16:50:31 -0400

    Add people

--- app/data/people.ts
+++ app/data/people.ts
@@ -18,6 +18,12 @@ export const peopleData: Section[] = [
         type: "grid",
         members: [
             { name: "Dr. Tomaso A. Poggio", affiliation: "Massachusetts Institute of Technology", image: "/people/poggio-240x300.jpg" },
+        ]
+    },
+    {
+        title: "Faculty Assistant",
+        type: "grid",
+        members: [
             { name: "Emma Downs", role: "Administrative Assistant to Dr. Tomaso Poggio", affiliation: "Massachusetts Institute of Technology", image: "/people/emma-downs.jpg" },
         ]
     },
